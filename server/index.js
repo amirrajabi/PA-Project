b8f268a322022ebb7587bb07c482350cbec19d86
Amir <amir@amirrajabi.com>
2019-11-04 17:02:13 +1100

    Add login, Token in server

--- server/index.js
+++ server/index.js
@@ -1,34 +1,55 @@
-// ---Config
+//---Config
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 
+const mongoose = require('mongoose');
 const config = require('config');
 const express = require('express');
 const _ = require('lodash');
 
-const { User } = require('./model/user');
+const {User} = require('./model/user');
 
 console.log(`*** ${String(config.get('Level')).toUpperCase()} ***`);
 
 const app = express();
 app.use(express.json());
 
+mongoose.set('useCreateIndex', true);
+
 app.post('/api/users', (req, res) => {
-    const body = _.pick(req.body, ['fullName', 'email', 'password']);
+    const body = _.pick(req.body, ['fullname', 'email', 'password']);
+
+    console.log(body);
+
     let user = new User(body);
 
     user.save().then((user) => {
         res.status(200).send(user);
     }, (err) => {
         res.status(400).json({
-            Error: `Something went wrong.${err}`
-        })
-    })
+            Error : `Something went wrong. ${err}`
+        });
+    });
 
+});
+
+app.post('/api/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).status(200).send(token);
+        }, (err) => {
+            res.status(400).json({
+                Error: `Something went wrong. ${err}`
+            });
+        });
+    })
 });
 
 app.listen(config.get('PORT'), () => {
-    console.log(`Server is running on port ${config.get('PORT')}`)
+    console.log(`Server is running on port ${config.get('PORT')}`);
 });
+
 
 
 // let newUser = new User({
--- server/model/user.js
+++ server/model/user.js
@@ -1,9 +1,18 @@
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
-const { mongoose } = require('./../db/mongoose');
+const {mongoose} = require('./../db/mongoose');
+
+const tokenOptions = {
+    type: String,
+    required: true
+};
 
 let UserSchema = new mongoose.Schema({
-    fullName: {
+    fullname: {
         type: String,
         required: true,
         minlength: 3,
@@ -12,18 +21,85 @@ let UserSchema = new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         minlength: 6,
         trim: true,
-        unique: true,
         validate: {
             validator: validator.isEmail,
-            message: `${value} is not valid email.`
+            message: '{Value} is not valid email'
         }
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        minlength: 6,
+        required: true
+    },
+    tokens:[{
+        access: tokenOptions,
+        token: tokenOptions
+    }]
+});
+
+UserSchema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'fullname', 'email']);
+}
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    let User = this;
+
+    return User.findOne({
+        email
+    }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+}
+
+UserSchema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+
+    let token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, config.get('JWT_SECRET')).toString();
+
+    user.tokens.push({
+        access,
+        token
+    });
+
+    return user.save().then(() => {
+        return token;
+    });
+}
+
+UserSchema.pre('save', function (next) {
+    let user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
     }
 });
 
